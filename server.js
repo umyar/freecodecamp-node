6f753cc684e1f56e50e5cdf2dbc7ddd262d24c60
Umiar Iusupov <umiar@umiar-iusupov.eks.giger.training>
2024-12-27 23:35:30 +0300

    vercel setup

--- server.js
+++ server.js
@@ -57,3 +57,5 @@ app.use(function(err, req, res, next) {
 const listener = app.listen(process.env.PORT || 3000, function () {
   console.log("Node.js listening on port " + listener.address().port);
 });
+
+module.exports = { app }
