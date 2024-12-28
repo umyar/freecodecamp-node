8edc35ff33d3e8a9625c3e74898b533f1608049f
Umiar Iusupov <umiar@umiar-iusupov.eks.giger.training>
2024-12-29 02:10:37 +0300

    next

--- myApp.js
+++ myApp.js
@@ -1,12 +1,10 @@
 let express = require('express');
 let app = express();
 
-console.log('Hello World');
-
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-app.use(express.static(__dirname + '/public'));
+app.use('/public', express.static(__dirname + '/public'));
 
 module.exports = app;
