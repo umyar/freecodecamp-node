b36979ab6ce0161563edc400fa4bb2417e9973a2
Umiar Iusupov <umiar@umiar-iusupov.eks.giger.training>
2024-12-29 02:13:27 +0300

    hotfix attempt

--- myApp.js
+++ myApp.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+// Normal usage
+app.use(express.static(__dirname + '/public'));
+
+// Assets at the /public route
 app.use('/public', express.static(__dirname + '/public'));
 
 module.exports = app;
