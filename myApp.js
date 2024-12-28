abe24216ef51d976bed02d4dc953926a41d68dbe
Umiar Iusupov <umiar@umiar-iusupov.eks.giger.training>
2024-12-29 02:27:22 +0300

    brute-force 3

--- myApp.js
+++ myApp.js
@@ -2,14 +2,14 @@ let express = require('express');
 let path = require('path');
 let app = express();
 
+// Normal usage
+app.use(express.static(__dirname + "/public"));
+
+// Assets at the /public route
+app.use("/public", express.static(__dirname + "/public"));
+
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
-
-// Normal usage
-// app.use(express.static(__dirname + '/public'));
-
-// Assets at the /public route
-app.use('/public', express.static(__dirname + '/public'));
 
 module.exports = app;
