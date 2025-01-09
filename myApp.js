1de12139424c10f7c6cd06f615199cb59702707b
Umiar Iusupov <umiarka@gmail.com>
2025-01-10 01:05:06 +0300

    json route

--- myApp.js
+++ myApp.js
@@ -1,10 +1,14 @@
 let express = require('express');
 let app = express();
 
-app.use("/public", express.static(__dirname + "/public"));
+app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/json', (req, res) => {
+  res.json({ message: 'Hello json' });
 });
 
 module.exports = app;
