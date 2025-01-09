34f8e1662dc71ed19cbe431d3402d04ed47c2308
Umiar Iusupov <umiarka@gmail.com>
2025-01-10 00:59:51 +0300

    revert test changes

--- myApp.js
+++ myApp.js
@@ -1,11 +1,10 @@
 let express = require('express');
 let app = express();
 
-// app.use("/", express.static(__dirname + "/public"));
-app.use("/public", express.static(__dirname + "/public/css-files"));
+app.use("/public", express.static(__dirname + "/public"));
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 module.exports = app;
