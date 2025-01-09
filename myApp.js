bc201dd4c619bb2c5ae24b3e0279ccc0eb94d815
Umiar Iusupov <umiarka@gmail.com>
2025-01-10 00:02:27 +0300

    css path in html page

--- myApp.js
+++ myApp.js
@@ -13,3 +13,16 @@ app.get('/', function (req, res) {
 });
 
 module.exports = app;
+
+/*
+let express = require('express');
+let app = express();
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+module.exports = app;
+*/
