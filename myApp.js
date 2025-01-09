ce5079e02f7eef5341453b685bc0c39bdf6a9bca
Umiar Iusupov <umiarka@gmail.com>
2025-01-10 00:31:24 +0300

    test

--- myApp.js
+++ myApp.js
@@ -1,28 +1,11 @@
 let express = require('express');
-let path = require('path');
 let app = express();
 
-// Normal usage
-// app.use(express.static(__dirname + "/public"));
-
-// Assets at the /public route
-app.use("/public", express.static(__dirname + "/public"));
+// app.use("/public", express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"));
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
 
 module.exports = app;
-
-/*
-let express = require('express');
-let app = express();
-
-app.use("/public", express.static(__dirname + "/public"));
-
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/public/index.html');
-});
-
-module.exports = app;
-*/
