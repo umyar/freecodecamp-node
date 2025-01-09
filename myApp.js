21efe19389983a4053b13b50192b97ea929f94dc
Umiar Iusupov <umiarka@gmail.com>
2025-01-10 00:33:17 +0300

    test 2

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 
 // app.use("/public", express.static(__dirname + "/public"));
-app.use(express.static(__dirname + "/public"));
+app.use("/", express.static(__dirname + "/public"));
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
