a5d490a9c02fc68bb40958160dcb1284e002c8a4
Umiar Iusupov <umiarka@gmail.com>
2025-01-17 01:32:23 +0300

    mongo setup 2

--- myApp.js
+++ myApp.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
-const createAndSavePerson = (done) => {
+const createAndSavePerson = done => {
   done(null /*, data*/);
 };
 
@@ -24,7 +26,7 @@ const findPersonById = (personId, done) => {
 };
 
 const findEditThenSave = (personId, done) => {
-  const foodToAdd = "hamburger";
+  const foodToAdd = 'hamburger';
 
   done(null /*, data*/);
 };
@@ -39,14 +41,14 @@ const removeById = (personId, done) => {
   done(null /*, data*/);
 };
 
-const removeManyPeople = (done) => {
-  const nameToRemove = "Mary";
+const removeManyPeople = done => {
+  const nameToRemove = 'Mary';
 
   done(null /*, data*/);
 };
 
-const queryChain = (done) => {
-  const foodToSearch = "burrito";
+const queryChain = done => {
+  const foodToSearch = 'burrito';
 
   done(null /*, data*/);
 };
