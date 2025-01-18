960146451b5940a59407c46358a0d19116e52a45
Umiar Iusupov <umiarka@gmail.com>
2025-01-18 20:25:02 +0300

    html file temp adding to the api dir

--- api/index.js
+++ api/index.js
@@ -41,7 +41,8 @@ app.use(bodyParser.urlencoded({ extended: "false" }));
 app.use(bodyParser.json());
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "views", "index.html"));
+  // res.sendFile(path.join(__dirname, "views", "index.html"));
+  res.sendFile(path.join(__dirname, "views_temp_copy_for_mongoose_section", "index.html"));
 });
 
 router.get("/file/*?", function (req, res, next) {
