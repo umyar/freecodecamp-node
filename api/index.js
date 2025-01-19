40aa85898bba8897321271deafea7782b428f09e
Umiar Iusupov <umiarka@gmail.com>
2025-01-20 00:36:16 +0300

    empty time param handling

--- api/index.js
+++ api/index.js
@@ -24,6 +24,12 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api', function (req, res) {
+  const now = new Date();
+
+  res.json({ unix: now.valueOf(), utc: now.toUTCString() });
+});
+
 app.get('/api/:time', (req, res) => {
   const { time } = req.params;
 
